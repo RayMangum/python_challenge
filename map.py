a71a67cd94824671a2beb86ec878c5e2fc09bd1c
Ray Mangum <raymangum@gmail.com>
2013-12-01 23:07:56 -0500

    Solved map using translate and stdin

--- map.py
+++ map.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
 import string
+import fileinput
 
 coded_message = r"g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
 trans_table = string.maketrans('abcdefghijklmnopqrstuvwxyz','cdefghijklmnopqrstuvwxyzab')
 
-print coded_message.translate(trans_table)
+for line in fileinput.input():
+    print line.translate(trans_table)
